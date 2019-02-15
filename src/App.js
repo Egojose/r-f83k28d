bb536ddd12654f760056b8c0681bfeee4ec6ac38
Egojose <josemicrobiologo@gmail.com>
2019-02-15 18:56:42 -0500

    se agrega funcion al contador

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0
+    }
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id="inc"onClick={this.counter.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+
+  counter(){
+    let contar = this.state.count;
+    let nuevaCuenta = contar + 1;
+    this.setState({
+      count: nuevaCuenta
+    });
   }
 }
 
